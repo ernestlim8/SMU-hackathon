c2743d1b0fb28135d3b769b4cb43cd3f85b6f711
Ebolaeater <53635104+Ebolaeater@users.noreply.github.com>
2021-08-11 20:37:49 +0800

    Add ability to view new text and old url

---
+++ Repapering_V1/src/taskpane/components/DifferenceRenderer.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+  
+export interface DifferencesProps {
+    newText: string;
+    oldURL: string
+}
+
+export default class DifferenceRenderer extends React.Component<DifferencesProps> {
+    render() {
+        const {newText, oldURL} = this.props;
+        return (
+            <div>
+                <h3 style={{textAlign: "left"}}>New Text:</h3>
+                <p>
+                    {newText}
+                </p>
+                <h3>Old URL:</h3>
+                <p>
+                    {oldURL}
+                </p>
+            </div>
+        );
+    }
+}
--- Repapering_V1/src/taskpane/components/HeroList.tsx
+++ Repapering_V1/src/taskpane/components/HeroList.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { HashRouter as Router, Link, Route } from "react-router-dom";
+// import {Link} from "react-router-dom"
+import DifferenceRenderer from "./DifferenceRenderer";
 
 export interface HeroListItem {
   icon: string;
@@ -14,15 +16,38 @@ export interface HeroListProps {
 export default class HeroList extends React.Component<HeroListProps> {
   render() {
     const { children, items, message } = this.props;
-
+    // console.log(items)
+    // const renderLinks = items.map((item, index) => {
+    //           console.log(item);
+    //           <li className="ms-ListItem" key={index}>
+    //             <i className={`ms-Icon ms-Icon--${item.icon}`}></i>
+    //               <Link to={`/${item.primaryText}`}>
+    //                 {item.primaryText}
+    //               </Link>
+    //               <Route exact path={`/${item.primaryText}`}
+    //                 component={() => {<DifferenceRenderer newText={item.primaryText} oldURL={item.primaryText}/>}}
+    //               >
+    //               </Route>
+                
+    //           </li>    
+    //         })
+    
     const listItems = items.map((item, index) => (
+      <Router>
       <li className="ms-ListItem" key={index}>
         <i className={`ms-Icon ms-Icon--${item.icon}`}></i>
-        <Link to="/1" className="ms-font-m ms-fontColor-neutralPrimary">
+        <Link to="/1">
           {item.primaryText}
         </Link>
+        <Route exact path='/1'>
+          <div style={{alignContent: "left"}}>
+          <DifferenceRenderer newText={item.primaryText} oldURL={item.primaryText}/>
+          </div>
+        </Route>
       </li>
+      </Router>
     ));
+
     return (
       <main className="ms-welcome__main">
         <h2 className="ms-font-xl ms-fontWeight-semilight ms-fontColor-neutralPrimary ms-u-slideUpIn20">{message}</h2>
